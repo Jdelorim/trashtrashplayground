9a40188cca9e31a6e34be5f24805c19c2ace917e
Jdelorim <joshuadelorimier@gmail.com>
2018-08-31 18:21:48 -0400

    setting up server

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -16,8 +16,8 @@ function draw() {
 function design1() {
     for(var w=0;w<width;w+=10){
         for(var h=0;h<height;h+=10) {
-        var n1 = map(noise(w,h,t1/1.3),0,1,-100,255);
-        var size = map(noise(h,w,t1/1.34),0,1,0,10);
+        var n1 = map(noise(w+t1*1.02,h),0,1,-100,255);
+        var size = map(noise(h*1.04,w),0,1,0,10);
             fill(n1);
             rect(w,h,size,size);
         }
