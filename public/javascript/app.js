8d374b0cf9204b229f0063201a1ac7a9cf765488
Jdelorim <joshuadelorimier@gmail.com>
2018-08-30 20:20:00 -0400

    added noise

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,4 +1,4 @@
-var time1 = 0;
+var t1 = 0;
 
 function setup() {
     var c = createCanvas(400,300, P2D);
@@ -16,11 +16,11 @@ function draw() {
 function design1() {
     for(var w=0;w<width;w+=10){
         for(var h=0;h<height;h+=10) {
-        var n1 = map(noise(w,h,time1/1.3),0,1,0,255);
-        var size = map(noise(h,w,time1/1.34),0,1,0,10);
+        var n1 = map(noise(w,h,t1/1.3),0,1,-100,255);
+        var size = map(noise(h,w,t1/1.34),0,1,0,10);
             fill(n1);
             rect(w,h,size,size);
         }
     }
-    time1+=0.08;
+    t1+=0.08;
 }
